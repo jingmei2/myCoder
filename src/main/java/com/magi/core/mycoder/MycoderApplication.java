2f012124f33f81de02bcfec231c34db4a16493e2
magi <751496272@qq.com>
2019-12-19 11:38:11 +0800

    frist commit

---
+++ src/main/java/com/magi/core/mycoder/MycoderApplication.java
@@ -0,0 +1,13 @@
+package com.magi.core.mycoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycoderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/magi/core/mycoder/MycoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magi.core.mycoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
